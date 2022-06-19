a645b2f4d240d5640b27e071c6c4a0f0a0f89691
Anastasia13 <sakirova.anastasi@yandex.ru>
2022-06-19 19:26:58 +0300

    cards

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,34 @@ let inputAbout = document.querySelector('.popup__input_type_about');
 let nameUser = document.querySelector('.profile__name');
 let aboutUser = document.querySelector('.profile__about-user');
 
+//карточки
+const initialCards = [
+    {
+      name: 'Китайская опера',
+      link: 'https://images.unsplash.com/photo-1653356572769-8a8d4048c91b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2075&q=80'
+    },
+    {
+      name: 'Гонг Конг',
+      link: 'https://images.unsplash.com/photo-1626067207555-5ee0e1ce04e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=437&q=80'
+    },
+    {
+      name: 'Киото',
+      link: 'https://images.unsplash.com/photo-1493780474015-ba834fd0ce2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1142&q=80'
+    },
+    {
+      name: 'Синдзюку',
+      link: 'https://images.unsplash.com/photo-1508504509543-5ca56440e013?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+    },
+    {
+      name: 'Каппадокия',
+      link: 'https://images.unsplash.com/photo-1632816049279-3ee6c809e629?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+    },
+    {
+      name: 'Пагода',
+      link: 'https://images.unsplash.com/photo-1571894793616-af0c7df4a68b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+    }
+  ];
+
 //открытие и закрытие формы редактирования профиля
 //закрытие клавишей Q
 /*function closePopupOnQ(e) {
@@ -61,3 +89,28 @@ function formSubmitHandler (evt) {
 }
 // Прикрепляем обработчик к форме:
 popupContainer.addEventListener('submit', formSubmitHandler);
+
+
+
+const elements = document.querySelector('.elements');
+const cardsTemplate = document.querySelector('#cards_template').content;
+
+function createCard(item) {
+    const cards = cardsTemplate.querySelector('.cards').cloneNode(true);
+    /*const btnLike = cards.querySelector('.cards__btn-like');
+    const btnDelete = cards.querySelector('.cards__delete-btn');*/
+    const cardsPhoto = cards.querySelector('.cards__photo');
+    const cardsCaption = cards.querySelector('.cards__caption');
+    cardsPhoto.alt = item.name;
+    cardsPhoto.src = item.link;
+    cardsCaption.textContent = item.name;
+    /*btnLike.addEventListener('click', activateLike);
+    btnDelete.addEventListener('click', deleteCard);*/
+    cardsPhoto.addEventListener('click', ()=>{openPreviewImage(item)});
+    return cards;
+  };
+
+  initialCards.forEach(function (item) {
+      const renderCards = createCard(item);
+      elements.prepend(renderCards);
+  });
